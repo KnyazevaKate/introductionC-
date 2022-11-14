0d89ee7bc6fdf84af8f46d16069d6a2b19a70cd3
«Князева <ladykate91@mail.ru>
2022-11-14 20:06:36 +0300

    Work 01

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1.С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+int a;
+a = 10;
+System.Console.WriteLine("Введите число");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+int b = a * a;
+System.Console.WriteLine($"{a}^2={b}");
+
+
+
+
+
