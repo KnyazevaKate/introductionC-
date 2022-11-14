457b2c2f2e152a8fe117a2c1193969d2e5c84c47
«Князева <ladykate91@mail.ru>
2022-11-14 20:45:18 +0300

    Work 01

---
+++ 04/Program.cs
@@ -0,0 +1,29 @@
+﻿// 4. По заданному с клавиатуры номеру дня недели вывести его название.
+double n = Convert.ToDouble(Console.ReadLine());
+string str = "";
+switch (n)
+{
+    case 1:
+        str = "Понедельник";
+        break;
+    case 2:
+        str = "Вторник";
+        break;
+    case 3:
+        str = "Среда";
+        break;
+    case 4:
+        str = "Четверг";
+        break;
+    case 5:
+        str = "Пятница";
+        break;
+    case 6:
+        str = "Суббота";
+        break;
+    case 7:
+        str = "Воскресенье";
+        break;
+}
+Console.WriteLine(str);
+Console.ReadKey();
---
+++ 05/Program.cs
@@ -0,0 +1,9 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int[] abc = new int[3];
+for (int i = 0; i < 3; i++)
+{
+    Console.Write($"input {i + 1} = ");
+    abc[i] = int.Parse(Console.ReadLine());
+}
+Array.Sort(abc);
+Console.WriteLine($"max = {abc[2]}, ave = {abc[1]}, min = {abc[0]}");
